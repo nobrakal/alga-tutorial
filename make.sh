72f2df02d46b8651c6353b339fba81b42f96ad97
Alexandre Moine <alexandre@moine.me>
2018-07-04 20:39:05 +0200

    give a try to standard prettyfier

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ pandoc -s -t html5 tutorial.tex > index.html
 sed -i '/<style.*>/,/<\/style>/d' index.html
 sed -i 's|</head>|</head><style>\n</style>|' index.html
 sed -i -e '/<style>/r main.css' index.html
-sed -i 's|</body>|<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?lang=hs\&skin=desert"></script>\n</body>|' index.html
+sed -i 's|</body>|<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?lang=hs"></script>\n</body>|' index.html
 sed -i 's|<pre class="sourceCode haskell">|<pre class="sourceCode haskell prettyprint">|' index.html
 sed -i 's|<code>|<code class="verbated">|g' index.html
 
